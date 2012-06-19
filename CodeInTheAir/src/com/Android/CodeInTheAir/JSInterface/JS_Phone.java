4c4ebfc76e0c40336df67645111223338ffa2fa4
Tiffany Chen <yuhan210@gmail.com>
2012-06-19 16:52:38 -0700

    Update master

--- CodeInTheAir/src/com/Android/CodeInTheAir/JSInterface/JS_Phone.java
+++ CodeInTheAir/src/com/Android/CodeInTheAir/JSInterface/JS_Phone.java
@@ -2,6 +2,9 @@ package com.Android.CodeInTheAir.JSInterface;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.widget.Toast;
 
 import com.Android.CodeInTheAir.Device.Actuate.System_Notify;
@@ -19,16 +22,24 @@ public class JS_Phone
 	{
 		System_Notify.vibrate();
 	}
-	
+		
 	public void vibrate(long ms)
 	{
 		System_Notify.vibrate(ms);
 	}
 	
+	public void ring(){
+			
+			Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+			Ringtone r = RingtoneManager.getRingtone(AppContext.context, notification);
+			r.play();
+
+	}
 	public void notify(String msg)
 	{
 		AlertDialog ad = new AlertDialog.Builder(AppContext.context).create();
 		ad.setCancelable(false);
+		ad.setTitle("Code in the air");
 		ad.setMessage(msg);
 		
 		ad.setButton("Ok", new DialogInterface.OnClickListener() {
